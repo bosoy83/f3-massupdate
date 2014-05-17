8b5c7a67b0b60bad1df854991247e9066f46df0a
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-05-17 10:03:24 -0400

    updated menu item adder

--- src/MassUpdate/Listener.php
+++ src/MassUpdate/Listener.php
@@ -1,32 +1,39 @@
-<?php 
+<?php
 namespace MassUpdate;
 
-class Listener extends \Prefab 
+class Listener extends \Prefab
 {
-    public function onSystemRebuildMenu( $event )
+
+    public function onSystemRebuildMenu($event)
     {
-    	if ($model = $event->getArgument('model'))
-    	{
-    		$root = $event->getArgument( 'root' );
-    		$update = clone $model;
-    	
-    		$update->insert(
-    				array(
-    						'type'	=> 'admin.nav',
-    						'priority' => 60,
-    						'title'	=> 'Mass Update',
-    						'icon'	=> 'fa fa-signal',
-        					'is_root' => false,
-    						'tree'	=> $root,
-    						'base' => '/admin/massupdate/',
-    				)
-    		);
-    		
+        if ($model = $event->getArgument('model'))
+        {
+            $root = $event->getArgument('root');
+            $update = clone $model;
+            
+            $update->insert(array(
+                'type' => 'admin.nav',
+                'priority' => 60,
+                'title' => 'Mass Update',
+                'icon' => 'fa fa-signal',
+                'is_root' => false,
+                'tree' => $root,
+                'base' => '/admin/massupdate/'
+            ));
+            
             $children = array(
-                    array( 'title'=>'List Updaters', 'route'=>'/admin/massupdate/updaters', 'icon'=>'fa fa-list' ),
-                    array( 'title'=>'Settings', 'route'=>'/admin/massupdate/settings', 'icon'=>'fa fa-cogs' ),
+                array(
+                    'title' => 'List Updaters',
+                    'route' => '/admin/massupdate/updaters',
+                    'icon' => 'fa fa-list'
+                ),
+                array(
+                    'title' => 'Settings',
+                    'route' => '/admin/massupdate/settings',
+                    'icon' => 'fa fa-cogs'
+                )
             );
-           	$update->addChildrenItems( $children, $root );
+            $update->addChildren($children, $root);
             
             \Dsc\System::instance()->addMessage('Mass Update added its admin menu items.');
         }
